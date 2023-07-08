22ec85ae4232daacb72c66a777b496e6659aec07
SamZec <samzec4@gmail.com>
2023-07-08 17:59:17 +0000

    2. Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,8 +19,8 @@ filename = "web_static_{}{}{}{}{}{}".format(year, month,
                                             day, hour, minute, second)
 command = "tar -cvzf versions/{}.tgz web_static".format(filename)
 
-env.hosts = ['ubuntu@35.190.155.240', 'ubuntu@104.196.65.224']
-env.key_filename = "~/.ssh/holberton"
+env.hosts = ['ubuntu@52.201.222.10', 'ubuntu@34.232.70.226']
+env.key_filename = "~/.ssh/id_rsa.pub"
 file_path = "versions/" + filename + ".tgz"
 
 
@@ -40,25 +40,31 @@ def do_deploy(archive_path):
     if not os.path.isfile(archive_path):
         return False
     filename = archive_path.split("/")[1]
-    name_no_ext = filename.split(".")[0]
+    name_ext = filename.split(".")[0]
     try:
         # Puts archive on server
         put(archive_path, '/tmp/')
         # Makes directory
-        if not files.exists('/data/web_static/releases/{}'.format(name_no_ext)):
-            run('sudo mkdir -p /data/web_static/releases/{}'.format(name_no_ext))
+        if not files.exists('/data/web_static/releases/{}'.format(name_ext)):
+            run('sudo mkdir -p /data/web_static/releases/{}'.format(name_ext))
         # Uncompresses archive
-        run('sudo tar -xvf /tmp/{}.tgz -C /data/web_static/releases/{}'.format(name_no_ext, name_no_ext))
+        run('sudo tar -xvf\
+                /tmp/{}.tgz -C\
+                /data/web_static/releases/{}'.format(name_ext, name_no_ext))
         # remove tgz file from temp
         run('sudo rm /tmp/{}.tgz'.format(name_no_ext))
         # move file to proper location
-        run('sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(name_no_ext, name_no_ext))
-        run('sudo rm -rf /data/web_static/releases/{}/web_static'.format(name_no_ext))
+        run('sudo mv /data/web_static/releases/{}/web_static/*\
+                /data/web_static/releases/{}/'.format(name_no_ext, name_ext))
+        run('sudo rm -rf\
+                /data/web_static/releases/{}/web_static'.format(name_ext))
         # Remove old symlink
         if files.exists('/data/web_static/current'):
             run('sudo rm -rf /data/web_static/current')
         # Create new symlink
-        run('sudo ln -s /data/web_static/releases/{} /data/web_static/current'.format(name_no_ext))
+        run('sudo ln -s\
+                /data/web_static/releases/{}\
+                /data/web_static/current'.format(name_no_ext))
         print("New version deployed!")
         return True
     except Exception:
